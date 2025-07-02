6b0228899220e9f7fe57555ac4e73f01c1fc1387
Enrico Meluziis <enricomlz@hotmail.it>
2025-07-02 09:07:18 +0200

    creates migration and model

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'body',
+        'is_draft',
+        'published_at'
+    ];
+
+    protected $casts = [
+        'is_draft'     => 'boolean',
+        'published_at' => 'date'
+    ]
+}
--- database/migrations/2025_07_02_065342_create_posts_table.php
+++ database/migrations/2025_07_02_065342_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->boolean('is_draft')->default(true);
+            $table->date('published_at')->nullable();
             $table->timestamps();
         });
     }
