737ef122f8b3e26bfd269076678ecbda626e3039
Enrico Meluziis <enricomlz@hotmail.it>
2025-07-03 21:56:03 +0200

    sets routes api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+// tutti, uno, inseire, put, patch, elimina
+Route::get('/posts', [PostController::class, '']);
+Route::get('/post/{id}');
+Route::post('/posts');
+Route::put('/post/{id}');
+Route::patch('/post/{id}');
+Route::delete('/post/{id}');
